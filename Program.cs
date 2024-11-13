18fff86f692dee8960862d6de3ace18af4b475a3
Louisoto <148911900+Louisoto@users.noreply.github.com>
2024-11-13 14:35:55 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace Projet_boogle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("tzst");
+            Console.WriteLine("tzt");
             Console.Read();
         }
     }
