6740ec0c0a6581fcab600ada6a32f6399508fa42
Quentin DE WOLF <quentin.de_wolf@edu.devinci.fr>
2024-11-15 13:12:59 +0100

    Attributs + constructeur

--- Joueur.cs
+++ Joueur.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,41 @@ namespace Projet_boogle
     {
         private string nom;
         private int score;
-        private 
+        private List<string> motsTrouvés;
+
+        public Joueur(string nom)
+        {
+            this.nom = nom;
+            this.score = 0;
+            this.motsTrouvés = new List<string>();
+        }
+
+        /// <summary>
+        ///  qui teste si le mot passé appartient déjà aux mots trouvés par le joueur pendant la partie
+        /// </summary>
+        /// <param name="mot"></param>
+        /// <returns></returns>
+        public bool Contain(string mot)
+        {
+            bool test = true;
+
+            return test;
+        }
+
+        /// <summary>
+        /// ajoute le mot dans la liste des mots déjà trouvés par le joueur au cours de la partie en modifiant le nombre d’occurrences si nécessaire
+        /// </summary>
+        /// <param name="mot"></param>
+        public void Add_Mot(string mot)
+        {
+
+        }
+
+        public string toString()
+        {
+            string resul = "";
+
+            return resul;
+        }
     }
 }
