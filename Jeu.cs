f1efa9bbddbad1a2c5f53007d96eb6c45737cf8a
Quentin DE WOLF <quentin.de_wolf@edu.devinci.fr>
2024-12-10 16:23:10 +0100

    Attribut tourEnCours

--- Jeu.cs
+++ Jeu.cs
@@ -8,12 +8,38 @@ namespace Projet_boogle
 {
     internal class Jeu
     {
+        #region Attributs
         private int tourEnCours;
+        private int tailleJeu;
+        private De[] jeu;
+        private int nbJoueurs;
+        private string[] nomJoueurs;
+        #endregion
+        #region Constructeur
+        public Jeu(int tailleJeu, int nbJoueurs)
+        {
+            this.tailleJeu = tailleJeu;
+            this.tourEnCours = 0;
+            this.nbJoueurs = nbJoueurs;
+            this.nomJoueurs = new string[nbJoueurs];
+            for (int i = 0; i < nbJoueurs; i++)
+            {
+                Console.Write("Quel est le nom du joueur " + i + " : ");
+                this.nomJoueurs[i] = Console.ReadLine();
+            }
+            this.jeu = new De[tailleJeu];
+            for (int i = 0; i < tailleJeu; i++)
+            {
+                jeu[i] = new De();
+            }
+        }
+        #endregion
         static void Main(string[] args)
         {
             Console.WriteLine("Allo, à l'huile");
             Console.WriteLine("Skibidi dop dop");
             Console.Read();
+            Console.WriteLine("Sélectionner vos options : ");
         }
     }
 }
