629de5ff13c06c4754d09775f4975c243d989d26
Quentin DE WOLF <quentin.de_wolf@edu.devinci.fr>
2024-12-13 19:10:47 +0100

    fonction test plateau

--- Plateau.cs
+++ Plateau.cs
@@ -11,16 +11,19 @@ namespace Projet_boogle
     {
         #region Attributs
         private int taille;
-        private De[] plateau;
+        private De[,] plateau;
         #endregion
         #region Constructeurs
         public Plateau(int tailleJeu)
         {
             this.taille = tailleJeu;
-            this.plateau = new De[tailleJeu*tailleJeu];
-            for (int i = 0; i < plateau.Length; i++)
+            this.plateau = new De[tailleJeu,tailleJeu];
+            for (int i = 0; i < tailleJeu; i++)
             {
-                plateau[i] = new De();
+                for (int j = 0; j < tailleJeu; j++)
+                {
+                    plateau[i, j] = new De();
+                }
             }
         }
         #endregion
@@ -32,13 +35,13 @@ namespace Projet_boogle
             {
                 for (int j = 0; j < this.taille; j++)
                 {
-                    resul += this.plateau[i + j].ToString();
+                    resul += this.plateau[i,j].ToString();
                 }
                 resul += "\n";
             }
             return resul;
         }
-        public bool Test_Plateau(string mot, int position = -1, List<int> posInvalides = new List<int>())
+        public bool Test_Plateau(string mot, Position[] posInvalides = null, Position posCourante = null,int compteur = 0)
         {
             if (mot == "")
             {
@@ -46,23 +49,45 @@ namespace Projet_boogle
             }
             else
             {
-                if (position == -1)
+                if (posCourante == null)
                 {
-                    for (int i = 0; i < this.plateau.Length; i++)
+                    posInvalides = new Position[mot.Length];
+                    for (int i = 0; i < this.plateau.GetLength(0); i++)
                     {
-                        if (this.plateau[i].Face_visible == mot[0])
+                        for (int j = 0; i < this.plateau.GetLength(1); j++)
                         {
-                            return Test_Plateau(mot.Substring(1), i, posInvalides.Add(i));
+                            if (this.plateau[i,j].Face_visible == mot[0])
+                            {
+                                posInvalides[compteur] = new Position(i,j);
+                                return Test_Plateau(mot.Substring(1), posInvalides, new Position(i,j), compteur + 1);
+                            }
                         }
                     }
                 }
                 else
                 {
-                    for (int i = 0; i < 8; i++)
+                    for (int i = -1; i <= 1; i++)
                     {
-                        for (int j = 0; j < posInvalides.Count; j++)
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            bool test = false;
+                            Position posTesté = new Position(posCourante.X + i, posCourante.Y + j);
+                            for (int k = 0; k < posInvalides.Length && !test; k++)
+                            {
+                                if (posTesté.X == posInvalides[i].X && posTesté.Y == posInvalides[j].Y)
+                                {
+                                    test = true;
+                                }
+                            }
+                            if (!test && posTesté.X > 0 && posTesté.X < this.plateau.GetLength(0) && posTesté.Y > 0 && posTesté.Y > this.plateau.GetLength(1) && this.plateau[posTesté.X, posTesté.Y].Face_visible == mot[0])
+                            {
+                                posInvalides[compteur] = posTesté;
+                                return Test_Plateau(mot.Substring(1), posInvalides, posTesté, compteur + 1);
+                            }
+                        }
                     }
                 }
+                return false;
             }
         }
         #endregion
